fbb528ab7fcd884ace20d93bb0aaa0c07f34e240
dharun122003 <dharunvignesh08@gmail.com>
2026-02-06 06:04:14 +0000

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<?php
+$server_ip = $_SERVER['SERVER_ADDR'];
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<title>StreamLine v1</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Welcome to StreamLine - v1</h1>
+<p>Server IP: <?php echo $server_ip; ?></p>
+</body>
+</html>
